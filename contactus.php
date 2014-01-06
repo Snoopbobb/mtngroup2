b75304d157697de0386aebc54ccb4eb0bcea7daa
Snoopbobb <snoopbobb@q.com>
2014-01-06 10:00:05 -0700

    initial commit

---
+++ contactus.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html>
+<head>
+<link href="images/mtngrouplogo.png" rel="shortcut icon" type="image/x-icon" />
+<link rel="stylesheet" type="text/css" href="css/main.css" />
+<meta name="viewport" content="width=device-width">
+<meta charset="utf-8">
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
+<title>The MTN Group Web Design and Development</title>
+</head>
+<body>
+ 
+ 
+ <div>
+ <h1 id="contact_header">
+ Use the form below to contact us, you can always call us at <br>719-209-9329 or email us at <a href="mailto:themtngroup@q.com" target="_blank" style="color:white";>themtngroup(at)q(dot)com</a> with any questions or concerns, thank you!
+ </h1>
+ </div>
+ 
+<div id="form"><br>
+<form name="contactform" method="post" action="contactussubmit.php">
+<table width="450px">
+<tr>
+ <td valign="top">
+  <label for="first_name">First Name *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="first_name" maxlength="50" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top"">
+  <label for="last_name">Last Name *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="last_name" maxlength="50" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top">
+  <label for="email">Email Address *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="email" maxlength="80" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top">
+  <label for="telephone">Telephone Number</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="telephone" maxlength="30" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top">
+  <label for="comments">Comments *</label>
+ </td>
+ <td valign="top">
+  <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+ </td>
+</tr>
+<tr>
+ <td colspan="2" style="text-align:center">
+  <input type="submit" value="Submit">   
+ </td>
+</tr>
+</table>
+</form>
+	
+</div>
+	
+	<div id='footer'>
+	<br>	<p>&copy The MTN Group 2013</p>
+	</div>
+</div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/smoothscroll.js"></script>
+</body>
+</html>
+
